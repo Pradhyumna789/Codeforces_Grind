e67b826407b1b01b80879ab0e441db78e0589158
Pradyumna <hariharshasripradhyumna@gmail.com>
2023-09-21 16:56:35 +0530

    committed today's work

--- 263A.cpp
+++ 263A.cpp
@@ -3,40 +3,31 @@
 #define ll long long
 using namespace std;
 
-int main()
-{
-    ll a[5][5];
+int main() {
+    ll a[5][5] = {0};
 
-    ll value = -1;
-    ll target_row = 5 / 2, target_col = 5 / 2;
-    ll cur_row = -1, cur_col = -1;
-
-    for(int i = 0; i < 5; i++)
-    {
-        for(int j = 0; j < 5; j++)
-        {
-            if(a[i][j] != 0)
-            {
-                value = a[i][j];
-                cur_row = i;
-                cur_col = j;
-            }
-
+    for(int i = 0; i < 5; i++) {
+         for(int j = 0; j < 5; j++) {
             cin >> a[i][j];
-
-        }
+         }
     }
 
-    cout << cur_row << " " << cur_col << endl;
+    cout << endl;
+    
+     ll curr_row = -1;
+     ll curr_col = -1;
+     ll target_row = 3;
+     ll target_col = 3; 
+     for(int i = 0; i < 5; i++) {
+         for(int j = 0; j < 5; j++) {
+            if(a[i][j] != 0) {
+                curr_col = j + 1;
+                curr_row = i + 1;
+            }
+         }
+    }
 
-    target_row = row_size / 2;
-    target_col = col_size / 2;
-
-    ll row_moves = abs(target_row - cur_row);
-    ll col_moves = abs(target_col - cur_col);
-    ll total_moves = row_moves + col_moves;
-
-    cout << total_moves << endl;
+    cout << abs(curr_row - target_row) + abs(curr_col - target_col) << endl;
 
     return 0;
 }
